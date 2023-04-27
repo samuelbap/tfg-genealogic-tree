6c74357e07b5390aaee4515a413d195d98e4f1a5
Samuel David Baptista Rivas <samueldavid.baptista@decathlon.com>
2023-04-27 16:55:48 +0200

    Move everything to main

---
+++ src/routes/components/Header.svelte
@@ -0,0 +1,32 @@
+<script>
+    import "@vtmn/css-dropdown"
+  //export let title;
+</script>
+
+<header class="flex items-center justify-between bg-gray-100 px-4 py-3">
+    <div class="flex items-center">
+      <img src=logo.png alt="Logo" class="h-8 mr-4">
+      <h1 class="text-lg font-bold">GenealogicTree</h1>
+    </div>
+    <div class="flex items-center">
+      <a href="/registros" class="mr-2 bg-gray-300 hover:bg-gray-400 text-gray-700 font-bold py-2 px-4 rounded">
+        Registros
+      </a>
+      <a href="/buscar" class="mr-2 bg-gray-300 hover:bg-gray-400 text-gray-700 font-bold py-2 px-4 rounded">
+        Buscar
+      </a>
+      <a href="/contacto" class="mr-2 bg-gray-300 hover:bg-gray-400 text-gray-700 font-bold py-2 px-4 rounded">
+        Contactanos
+      </a>
+      <a href="/login" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Log In
+      </a>
+    </div>
+  </header>
+  
+  <style>
+    /* Importa los estilos de Tailwind */
+    @import "tailwindcss/base";
+    @import "tailwindcss/components";
+    @import "tailwindcss/utilities";
+  </style>
