f6ae03799143a211d872110c95ca76c067374a88
Samuel David Baptista Rivas <samueldavid.baptista@decathlon.com>
2023-05-12 16:01:18 +0200

    fix: Restructure two of baclenf and fontend

--- restructure/backend/src/routes/routes.js
+++ restructure/backend/src/routes/routes.js
@@ -1,31 +1,46 @@
 // backend/src/routes/routes.js
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const documentController = require('../controllers/documentController');
-const documentLocationController = require('../controllers/documentLocationController');
-const personController = require('../controllers/personController');
+const {documentController} = require("../controllers/documentController");
+const documentLocationController = require("../controllers/documentLocationController");
+const personController = require("../controllers/personController");
 
 // Document routes
-router.get('/documents', documentController.getAllDocuments);
-router.post('/documents', documentController.createDocument);
-router.get('/documents/:id', documentController.getDocumentById);
-router.put('/documents/:id', documentController.updateDocument);
-router.delete('/documents/:id', documentController.deleteDocument);
+router.get("/documents", documentController.getAllDocuments);
+router.post("/documents", documentController.createDocument);
+router.get("/documents/:id", documentController.getDocumentById);
+router.put("/documents/:id", documentController.updateDocument);
+router.delete("/documents/:id", documentController.deleteDocument);
 
 // DocumentLocation routes
-router.get('/documentlocations', documentLocationController.getAllDocumentLocations);
-router.post('/documentlocations', documentLocationController.createDocumentLocation);
-router.get('/documentlocations/:id', documentLocationController.getDocumentLocationById);
-router.put('/documentlocations/:id', documentLocationController.updateDocumentLocation);
-router.delete('/documentlocations/:id', documentLocationController.deleteDocumentLocation);
+router.get(
+  "/documentlocations",
+  documentLocationController.getAllDocumentLocations
+);
+router.post(
+  "/documentlocations",
+  documentLocationController.createDocumentLocation
+);
+router.get(
+  "/documentlocations/:id",
+  documentLocationController.getDocumentLocationById
+);
+router.put(
+  "/documentlocations/:id",
+  documentLocationController.updateDocumentLocation
+);
+router.delete(
+  "/documentlocations/:id",
+  documentLocationController.deleteDocumentLocation
+);
 
 // Person routes
-router.get('/persons', personController.getAllPersons);
-router.post('/persons', personController.createPerson);
-router.get('/persons/:id', personController.getPersonById);
-router.put('/persons/:id', personController.updatePerson);
-router.delete('/persons/:id', personController.deletePerson);
+router.get("/persons", personController.getAllPersons);
+router.post("/persons", personController.createPerson);
+router.get("/persons/:id", personController.getPersonById);
+router.put("/persons/:id", personController.updatePerson);
+router.delete("/persons/:id", personController.deletePerson);
 
 // DocumentProtagonist routes
 // Add your documentProtagonist routes here
@@ -35,6 +50,5 @@ router.delete('/persons/:id', personController.deletePerson);
 
 // DocumentChildren routes
 // Add your documentChildren routes here
-
 
 module.exports = router;
